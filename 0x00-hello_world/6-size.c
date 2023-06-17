92bdfd660fd6b71de4767a51696fc9a237372a3b
Gabriel <cobrielisuekebho9270@gmail.com>
2023-06-16 21:39:10 -0700

    C programming script

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,9 +12,11 @@ int main(void)
 	long int k;
 	long long int l;
 	float m;
+
 	printf("Size of a char: %ld byte(s)\n", sizeof(i));
-	printf("Siz of an int: %ld byte(s)\n", sizeof(j));
+	printf("Size of an int: %ld byte(s)\n", sizeof(j));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(k));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(l));
 	printf("Size of a float: %ld byte(s)\n", sizeof(m));
+	return (0);
 }
