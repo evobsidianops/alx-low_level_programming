36e2f1465f20b025975c3b7994f57cedd1364f76
Gabriel <cobrielisuekebho9270@gmail.com>
2023-06-16 21:30:23 -0700

    C programming script

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+	int j;
+	long int k;
+	long long int l;
+	float m;
+	printf("Size of a char: %ld byte(s)\n", sizeof(i));
+	printf("Siz of an int: %ld byte(s)\n", sizeof(j));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(k));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(l));
+	printf("Size of a float: %ld byte(s)\n", sizeof(m));
+}
