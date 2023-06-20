960eccc8a0bb9340ef24791e567ba9778cdc8509
Gabriel <cobrielisuekebho9270@gmail.com>
2023-06-19 21:59:45 -0700

    more scripts

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: return success (0)
+ */
+int main(void)
+{
+	int n;
+
+	n = 48;
+	while (n != 59)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
