af05078ca4530ef570cfebbdd9f10fb8e72136fe
Gabriel <cobrielisuekebho9270@gmail.com>
2023-06-22 11:30:15 -0700

    debugging functions

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - test function positive_and_negative() output
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	int j;
+
+	j = 0;
+	positive_or_negative(j);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+Learning and debugging c scripts
