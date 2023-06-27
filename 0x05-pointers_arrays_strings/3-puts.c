e5171d714656f9fa0ccc6bcf3f38c62b844f236e
Gabriel <cobrielisuekebho9270@gmail.com>
2023-06-27 04:01:09 -0700

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Entry point
+ * @str: pointer character parameter
+ *
+ * Description: Prints to stdout input from @str
+ * Return: returns a string
+ */
+void _puts(char *str)
+{
+	 puts(str);
+}
