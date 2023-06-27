069d8d89315d13a85889b487d7bd6f82a8017eba
Gabriel <cobrielisuekebho9270@gmail.com>
2023-06-27 03:49:37 -0700

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - Count the number of characters
+ * @s: pointer character parameter
+ *
+ * Description: Counts the number of character passed as input
+ * Return: return type int
+ */
+int _strlen(char *s)
+{
+	int p;
+
+	p = strlen(s);
+	return (p);
+}
