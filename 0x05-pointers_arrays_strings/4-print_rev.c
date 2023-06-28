ac1d6b2ab43bf2059f7df7aa35157962c69df1e6
Gabriel <cobrielisuekebho9270@gmail.com>
2023-06-27 21:03:55 -0700

    pointer script

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_rev - Entry point
+ * @s: pointer character param
+ *
+ * Description: reverses a string
+ * Return: return null
+ */
+void print_rev(char *s)
+{
+	int len;
+	int i;
+	int j;
+
+	len = strlen(s);
+
+	for (i = 0, j = len - 1; i < j; i++, j--)
+	{
+		char p;
+
+		p = s[i];
+		s[i] = s[j];
+		s[j] = p;
+
+	}
+}
