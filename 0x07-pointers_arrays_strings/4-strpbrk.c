7465952773b7d7a6793fbd3c5b86c7207b119e79
Gabriel <cobrielisuekebho9270@gmail.com>
2023-07-03 15:39:23 -0700

    more pointers

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -5,7 +5,7 @@
 /**
  * _strpbrk - Entry point
  * @s: strinr of bytes
- * @accept: string to be searched for 
+ * @accept: string to be searched for
  *
  * Description: locates the first occurrence in the string s
  * Return: returns apointer to the byte in s
