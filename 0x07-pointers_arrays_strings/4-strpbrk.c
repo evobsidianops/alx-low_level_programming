6522bcd38fe4fb8e3ee4a59850e0ecc4d6e88728
Gabriel <cobrielisuekebho9270@gmail.com>
2023-07-03 15:37:18 -0700

    more pointers

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strpbrk - Entry point
+ * @s: strinr of bytes
+ * @accept: string to be searched for 
+ *
+ * Description: locates the first occurrence in the string s
+ * Return: returns apointer to the byte in s
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *ret;
+
+	ret = strpbrk(s, accept);
+	return (ret);
+}
