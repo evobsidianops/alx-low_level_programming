a0d3cfc7422854be7a30557dadb6ada97834e175
Gabriel <cobrielisuekebho9270@gmail.com>
2023-07-18 04:55:16 -0700

    processor macro script

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes dog names
+ * @d: dog struct
+ * @name: dogs name
+ * @age: dog age
+ * @owner: dogs owner
+ *
+ * Return: return void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
