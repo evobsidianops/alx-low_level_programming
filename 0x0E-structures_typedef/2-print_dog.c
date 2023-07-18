0d4bb8c517c1d4c6020a48d92325f84a6650b7be
Gabriel <cobrielisuekebho9270@gmail.com>
2023-07-18 06:15:47 -0700

    processor macro script

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,18 +9,20 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-	{
 		return;
-	}
 
 	if (d->name == NULL)
-	{
-		d->name = "(nil)";
-	}
+		printf("Name: (nil)\n");
 	else
-	{
-		printf("%s\n", d->name);
-		printf("%.6f\n", d->age);
-		printf("%s\n", d->owner);
-	}
+		printf("Name: %s\n", d->name);
+
+	if (d->age < 0)
+		printf("Age: (nil)\n");
+	else
+		printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 }
