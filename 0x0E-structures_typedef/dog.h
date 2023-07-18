d836e4b4fca02ce1a76454a63b1f0106d2a9a02f
Gabriel <cobrielisuekebho9270@gmail.com>
2023-07-18 04:44:45 -0700

    processor macro script

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef STRUCT_H
+#define STRUCT_H
+
+/**
+ * struct dog - Dog features
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: dogs owner
+ *
+ * Description: Prints dog's name age and owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
