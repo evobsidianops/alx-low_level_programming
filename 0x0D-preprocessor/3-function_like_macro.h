fa037bfeed1971cf916f164f73b28a5c8c36f3b6
evobsidianops <132454145+evobsidianops@users.noreply.github.com>
2023-07-25 21:55:34 +0100

    Delete 0x0D-preprocessor directory

--- 0x0D-preprocessor/3-function_like_macro.h
+++
@@ -1,6 +0,0 @@
-#ifndef FUNCTION_ABS_H
-#define FUNCTION_ABS_H
-
-#define ABS(x) ((x) >= (0) ? (x) : -(x))
-
-#endif
--- 0x0D-preprocessor/4-sum.h
+++
@@ -1,6 +0,0 @@
-#ifndef FUNCTION_SUM_MACRO_H
-#define FUNCTION_SUM_MACRO_H
-
-#define SUM(x, y) ((x) + (y))
-
-#endif
