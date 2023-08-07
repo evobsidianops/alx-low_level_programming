e747e307556fc92408a62a2f14cb1b724e0f1eaf
evobsidianops <cobrielisuekebho9270@gmail.com>
2023-08-07 11:01:39 -0700

    argc and argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: argcount
+ * @argv: argument ector
+ *
+ * Description: Prints its name
+ * Return: returns its name
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,10 @@
+#ifndef ARG_H
+#define ARG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]);
+int _putchar(char c);
+
+#endif
