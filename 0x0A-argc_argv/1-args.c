7f52a5e47adcf627d3ba73d5ac3b8d86c5638f33
evobsidianops <cobrielisuekebho9270@gmail.com>
2023-08-07 11:14:05 -0700

    argc and argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Description: returns number of argument count
+ * Return: returns the number count
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
