bf8390d7243fab8071d9dd3b8bf6a8265f47c2a1
evobsidianops <cobrielisuekebho9270@gmail.com>
2023-08-09 02:47:29 -0700

    memory allocation

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,7 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 	unsigned int i;
 
-	ptr = malloc((size + 1) * sizeof(char));
+	ptr = malloc(size  * sizeof(char));
 	i = 0;
 	if (ptr == NULL)
 	{
