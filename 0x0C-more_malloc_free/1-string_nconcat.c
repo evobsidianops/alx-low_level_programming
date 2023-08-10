a848320d4d3bef9f49a4ae4c62097aa2baffc15b
evobsidianops <cobrielisuekebho9270@gmail.com>
2023-08-10 09:15:19 -0700

    str concat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * string_nconcat - concatenate 2 string
+ * @s1: irst param
+ * @s2: src
+ * @n: number of interger
+ *
+ * Return: returns a pointer to the newly allocated mem
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ptr;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	ptr = malloc(sizeof(s1) + sizeof(s2) + 1);
+	if (ptr != NULL)
+	{
+		 strcat(ptr, s1);
+		 if (n >= strlen(s2))
+		 {
+			 strcat(ptr, s2);
+		 }
+		strncat(ptr, s2, n);
+	}
+	else
+	{
+		free(ptr);
+		return (NULL);
+	}
+	return (ptr);
+}
