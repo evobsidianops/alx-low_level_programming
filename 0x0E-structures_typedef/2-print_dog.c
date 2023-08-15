fa007fd10841c53f19e6761cff4c156223f691bd
evobsidianops <cobrielisuekebho9270@gmail.com>
2023-08-15 12:43:46 -0700

    fix control flow issue

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,24 +10,11 @@ void print_dog(struct dog *d)
 	{
 		d->name = "(nil)";
 	}
-	else
-	{
-		printf("Name: %s\n", d->name);
-	}
-	if (d->age == 0)
-	{
-		printf("Age: (nil)\n");
-	}
-	else
-	{
-		printf("Age: %.6f\n", d->age);
-	}
+	printf("Name: %s\n", d->name);
+	printf("Age: %.6f\n", d->age);
 	if (d->owner == NULL)
 	{
 		d->owner = "(nil)";
 	}
-	else
-	{
-		printf("Owner: %s\n", d->owner);
-	}
+	printf("Owner: %s\n", d->owner);
 }
