3e547594c9412fef960eebed0102550ab471382f
evobsidianops <cobrielisuekebho9270@gmail.com>
2023-08-15 10:40:50 -0700

    created a struct to form characteristics of dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,12 @@
+/**
+ * struct dog - New character for the dog
+ * @nmae: name of the dog
+ * @age: age of the dog
+ * @owner: dog's owner
+ */
+
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
