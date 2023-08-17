3a1526fbc8f0ea61be8063d678e2a301f3a6ddf1
evobsidianops <cobrielisuekebho9270@gmail.com>
2023-08-17 08:00:12 -0700

    code that executes function passed an parameters

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * array_iterator - iterates over the array
+ * @array: array containing param
+ * @size: the size of the array
+ * @action: pointer to the function
+ *
+ * Description: executes a function passed as an argument
+ * Return: returns the executed function
+ */
+void array_iterator(int *array, size_t size,  void (*action)(int))
+{
+	unsigned int i;
+
+	if (action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
