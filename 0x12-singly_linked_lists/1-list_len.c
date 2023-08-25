ab2ed3ba8fe2a843b2f6340a3f4817f04a4235ff
evobsidianops <cobrielisuekebho9270@gmail.com>
2023-08-25 21:54:17 +0100

    print the number of node in the linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - The length of the nodes
+ * @h: pointer to the first node
+ *
+ * Return: returns the length of the linked list
+ */
+size_t list_len(const list_t *h)
+{
+	int len;
+
+	len = 0;
+	while (h != NULL)
+	{
+		h = h-> next;
+		len++;
+	}
+	return (len);
+}
