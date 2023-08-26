14a2332657f1526dade6418384251bae9d64a229
evobsidianops <cobrielisuekebho9270@gmail.com>
2023-08-26 04:24:23 +0100

    function to print first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
  *
  * Return: void
  */
-void __attribute__ ((constructor)) first()
+void __attribute__ ((constructor)) run()
 {
 	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
