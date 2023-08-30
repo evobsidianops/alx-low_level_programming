cd64386606f4a430f2a959c1b08f6325b52745f3
evobsidianops <cobrielisuekebho9270@gmail.com>
2023-08-30 07:33:49 +0100

    fixing edge cases

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,9 +7,16 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *mem;
+	char f[7] = "Freed!";
+	int i;
 
 	if (*head == NULL)
-		return;
+	{
+		for (i = 0; i < 7; i++)
+		{
+			_putchar(f[i]);
+		}
+	}
 	while (*head != NULL)
 	{
 		mem = (*head)->next;
